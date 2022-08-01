cc0d5edc60507b99f6f437821e670c03a344574e
Marsel Asatov <asamarsel@mail.ru>
2022-08-01 15:55:11 +0300

    Lesson 8 passed

--- src/main/java/com/dmdev/spring/ApplicationRunner.java
+++ src/main/java/com/dmdev/spring/ApplicationRunner.java
@@ -6,6 +6,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ApplicationRunner {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
-        System.out.println(context.getBean("p4", ConnectionPool.class));
+        System.out.println(context.getBean("p1", ConnectionPool.class));
     }
 }
--- src/main/java/com/dmdev/spring/pool/ConnectionPool.java
+++ src/main/java/com/dmdev/spring/pool/ConnectionPool.java
@@ -7,12 +7,16 @@ public class ConnectionPool {
     private final String username;
     private final Integer poolSize;
     private final List<String> args;
-    private final Map<String, Object> properties;
+    private Map<String, Object> properties;
 
     public ConnectionPool(String username, Integer poolSize, List<String> args, Map<String, Object> properties) {
         this.username = username;
         this.poolSize = poolSize;
         this.args = args;
+        this.properties = properties;
+    }
+
+    public void setProperties(Map<String, Object> properties) {
         this.properties = properties;
     }
 }
