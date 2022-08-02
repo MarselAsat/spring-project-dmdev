713865a208eb9184d489fad08e90d0fab79919fd
Marsel Asatov <asamarsel@mail.ru>
2022-08-02 11:04:41 +0300

    Lesson  12 passed

--- src/main/java/com/dmdev/spring/ApplicationRunner.java
+++ src/main/java/com/dmdev/spring/ApplicationRunner.java
@@ -2,10 +2,15 @@ package com.dmdev.spring;
 
 import com.dmdev.spring.pool.ConnectionPool;
 import com.dmdev.spring.repository.CompanyRepository;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
     public static void main(String[] args) {
+        String value = "hello";
+        System.out.println(CharSequence.class.isAssignableFrom(value.getClass()));
+        System.out.println(BeanFactoryPostProcessor.class.isAssignableFrom(value.getClass()));
+
         try(ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml");) {
             ConnectionPool connectionPool = context.getBean("p1", ConnectionPool.class);
             CompanyRepository companyRepository = context.getBean("companyRepository", CompanyRepository.class);
